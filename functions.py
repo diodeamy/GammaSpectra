47041801dbd51e97adbbcb6a80798b0f49e99a4a
diodeamy <99472600+diodeamy@users.noreply.github.com>
2023-06-18 12:42:08 +0200

    changed a lot

--- functions.py
+++ functions.py
@@ -36,3 +36,34 @@ def savetocsv(daframe, dipath):
     '''
     
     daframe.to_csv(dipath)
+    
+def allthewayup(dataset, peakval1, peakval2, peakwidth = 150):
+    '''
+    I want this function to take the dataset, seek the highest two values of "average", which are the counts;
+    and depending on the two peakvalues selected to make the distrinction, to establish the conversion rate;
+    from "ADC channel" to energy in electron volts (eVs).
+    
+    It will have a quirk for the Co60 source, where the identified peaks have to be in a specific region above;
+    the 12k channel region (this is clearly where they are by comparing the data obtained with literature)
+    '''
+    df_sort = dataset.sort_values(by=['average'], 
+                              ascending=False, inplace=False)[['ADC channel', 'average', 'error']]
+    
+    # we first find the largest peak
+    dp1 = df_sort.iloc[0]
+    print(dp1)
+    
+    # we then want to remove the peak already found
+    dp2 = df_sort[df_sort['ADC channel'] > peakwidth].iloc[0]
+    print(dp2)
+    
+    # now we can find the ratio of conversion
+    # i.e. this amount is how many eVs one channel is equivalent to
+    ratio = (peakval2 - peakval1)/(dp2['ADC channel'] - dp1['ADC channel'])
+    print(ratio)
+    
+    # all we are left to do now is multiply each channel value by this ratio and BAM!
+    datasetc = dataset.copy()
+    datasetc['energy'] = datasetc['ADC channel'].apply(lambda x: x*ratio)
+        
+    return datasetc
